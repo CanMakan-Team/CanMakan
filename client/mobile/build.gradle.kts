d1b00403fa22ef80981a71558e14a89b58f7c4ed
K4i-Z3r <kamaruzaman.khairulanwar@outlook.com>
2026-07-29 20:34:04 +0800

    Setting up the Gradle Environment and Manifest File (Mobile)

--- client/mobile/build.gradle.kts
+++ client/mobile/build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where common configuration options for all
 // modules in this project are declared.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "9.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
