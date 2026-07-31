2f8e1ab882a82228f6dc8db04c805f4693daec15
Wong Su Hui Amelia <ameliaw2903@gmail.com>
2026-07-31 16:23:23 +0800

    Update DietaryRestrictionViewModel.kt

--- client/mobile/app/src/main/java/sg/edu/nus/iss/canmakan/features/dietaryprofile/restrictions/DietaryRestrictionViewModel.kt
+++ client/mobile/app/src/main/java/sg/edu/nus/iss/canmakan/features/dietaryprofile/restrictions/DietaryRestrictionViewModel.kt
@@ -1,4 +1,4 @@
-package sg.edu.nus.iss.canmakan.features.userprofile
+package sg.edu.nus.iss.canmakan.features.dietaryprofile.restrictions
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
