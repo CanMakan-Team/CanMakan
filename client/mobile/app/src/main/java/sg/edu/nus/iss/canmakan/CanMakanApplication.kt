cadcb11b1deb786e9461db191b1bbd08c52ba1d0
K4i-Z3r <kamaruzaman.khairulanwar@outlook.com>
2026-08-02 20:03:43 +0800

    Amendment to BarcodeValidation.java

--- client/mobile/app/src/main/java/sg/edu/nus/iss/canmakan/CanMakanApplication.kt
+++ client/mobile/app/src/main/java/sg/edu/nus/iss/canmakan/CanMakanApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
+import sg.edu.nus.iss.canmakan.BuildConfig
 
 /**
  * Application that sets up Timber in the DEBUG BuildConfig.
