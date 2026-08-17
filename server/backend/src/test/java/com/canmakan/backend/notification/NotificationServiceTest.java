7cac6b6c818567ad18b7c976c765fe139a9ec8d6
Codemelia <ameliaw2903@gmail.com>
2026-08-18 07:26:44 +0800

    backend notification test updates

--- server/backend/src/test/java/com/canmakan/backend/notification/NotificationServiceTest.java
+++ server/backend/src/test/java/com/canmakan/backend/notification/NotificationServiceTest.java
@@ -10,7 +10,11 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.canmakan.backend.user.UserAccountRepository;
+import com.canmakan.backend.notification.dto.UserNotificationResponse;
+import com.canmakan.backend.notification.exception.NotificationNotFoundException;
+import com.canmakan.backend.notification.model.NotificationType;
+import com.canmakan.backend.notification.model.UserNotification;
+import com.canmakan.backend.user.repository.UserAccountRepository;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
